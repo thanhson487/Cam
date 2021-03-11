205f413aad600760e83791ea883e9dd19ff09385
thanhson487 <thanhsonptitit@gmail.com>
2021-03-11 13:23:33 +0700

    final 1

---
+++ cam.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.filter').click(function(){
+        $('.header__menu').toggle();
+       
+    })
+    $('.fa-bars').click(function(){
+        $('.profile-container').toggle();
+       
+    })
+  });
